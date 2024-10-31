2acd5e432f34ef01ef7cab2378ec9c516006f1a8
EliasMN <elias010493@gmail.com>
2024-10-31 19:54:00 +0100

    Finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,69 @@
 let keyPiano = {
-  'do': 'audio/Do.mp3'
+  'do': 'src/audio/1.mp3',
+  'do#': 'src/audio/a.mp3',
+  're': 'src/audio/2.mp3',
+  're#': 'src/audio/b.mp3',
+  'mi': 'src/audio/3.mp3',
+  'fa': 'src/audio/4.mp3',
+  'fa#': 'src/audio/c.mp3',
+  'sol': 'src/audio/5.mp3',
+  'sol#': 'src/audio/d.mp3',
+  'la': 'src/audio/6.mp3',
+  'la#': 'src/audio/e.mp3',
+  'si': 'src/audio/7.mp3'
 }
 
-document.addEventListener('keydown', (event) => {
-  // <div class="key white-key" data-key="do"></div>
-  playPiano(event.key);
-})
 
-let audio = new Audio();
+document.addEventListener('keydown', (event) => {
+
+  let keyDown = event.key.toLowerCase();
+
+  switch (keyDown) {
+    case "q":
+      playPiano("do");
+    break;
+    case "2":
+      playPiano("do#");
+    break;
+    case "w":
+      playPiano("re")
+    break;
+    case "3":
+      playPiano("re#")
+    break;
+    case "e":
+      playPiano("mi")
+    break;
+    case "r":
+      playPiano("fa")
+    break;
+    case "5":
+      playPiano("fa#")
+    break;
+    case "t":
+      playPiano("sol")
+    break;
+    case "6":
+      playPiano("sol#")
+    break;
+    case "y":
+      playPiano("la")
+    break;
+    case "7":
+      playPiano("la#")
+    break;
+    case "u":
+      playPiano("si")
+    break;
+  }
+});
+
 function playPiano(key){
-  // 1º option
-  //let audio = new Audio(keyPiano[key]);
-  //audio.play();
-  // 2º option
-  //audio.src = keyPiano[key].source;
-  //audio.play();
+  let audio = new Audio(keyPiano[key]);
+  audio.play();
+  let keyElement = document.getElementById(key);
+  keyElement.classList.add("active");
+  setTimeout(() => {
+    keyElement.classList.remove("active");
+  }, 250);
 }
